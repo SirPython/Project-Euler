04724cf11853dd92f4023c14be31d458a8855d35
SirPython <SirPython@github.noreply.com>
2020-07-28 11:26:02 -0400

    change names

---
+++ 006.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int sum_of_squares = 0;
+    for(int i = 1; i <= 100; i++) {
+        sum_of_squares += i * i;
+    }
+
+    int square_of_sum = ((100 * 99) / 2) * ((100 * 99) / 2);
+
+    printf("%d %d\n", sum_of_squares, square_of_sum);
+}
